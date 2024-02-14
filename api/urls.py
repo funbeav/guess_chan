b323d824bc4d3e4384ade79c097ee11743c411d7
Ivan Sidorov <61821868+iisidorov@users.noreply.github.com>
2024-02-14 11:26:44 +0300

    fix api urls aliases

--- api/urls.py
+++ api/urls.py
@@ -5,11 +5,11 @@ from api import views
 
 urlpatterns = [
     path('', include('rest_framework.urls')),
-    path('profile/', views.ProfileViewSet.as_view({'get': 'retrieve'}), name='user'),
+    path('profile/', views.ProfileViewSet.as_view({'get': 'retrieve'}), name='profile'),
     path('users/', views.UserListViewSet.as_view({'get': 'list'}), name='user'),
 
     path('play/', views.GameViewSet.as_view({
         'get': 'get_attempt',
         'post': 'get_answer_result',
-    }), name='user'),
+    }), name='play'),
 ]
